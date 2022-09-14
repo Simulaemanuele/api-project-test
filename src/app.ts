367a3608b39690fe2a453df675c15a80c8b2c322
Emanuele Simula <simulaemanuele@gmail.com>
2022-09-14 17:54:10 +0200

    Add POST space_facts route

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,20 @@ import prisma from './lib/prisma/client'
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/planets", async (request, response) => {
     // response.json([{ name: "Mercury" }, { name: "Venus" }]);
     const planets = await prisma.planet.findMany();
 
     response.json(planets)
+});
+
+app.post("/planets", async (request, response) => {
+    // response.json([{ name: "Mercury" }, { name: "Venus" }]);
+    const planet = request.body;
+
+    response.status(201).json(planet)
 });
 
 export default app;
