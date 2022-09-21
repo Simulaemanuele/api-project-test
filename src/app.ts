e4ec6291f8a5f56d8c378ef9f4ec8e266ff561de
Emanuele Simula <simulaemanuele@gmail.com>
2022-09-21 11:29:53 +0200

    Configure Multer to restrict file uploads.

--- src/app.ts
+++ src/app.ts
@@ -99,7 +99,6 @@ app.post(
     "/planets/:id(\\d+)/photo",
     upload.single("photo"),
     async (request, response, next) => {
-        console.log("request.file", request.file);
 
         if (!request.file) {
             response.status(400);
